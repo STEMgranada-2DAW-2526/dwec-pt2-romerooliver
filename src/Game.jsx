edd592adeca549849a9de2792d63ce6ab0004e50
romerooliver <oliverromero001.alu@stemgranada.com>
2025-11-25 10:53:45 +0100

    configurando gameProvider

---
+++ src/Game.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function Game() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Game
--- src/GameProvider.jsx
+++ src/GameProvider.jsx
@@ -1,5 +1,11 @@
 import { useContext } from 'react';
 
 export default function GameProvider() {
-    
+    function gameReducer(state, action) {
+
+        let outputState = state;
+        { }
+
+        return outputState;
+    }
 }
